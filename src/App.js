4072ca47e614aa622ad61809bc4ee70f038f238f
gtz.jesus <gtz.jesus@outlook.com>
2023-08-16 11:45:46 -0600

    Creating API for quiz👨🏼‍💻

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import DateCounter from './DateCounter.js';
+import Main from './Main.js';
+import Header from './Header.js';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action Unknown');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
